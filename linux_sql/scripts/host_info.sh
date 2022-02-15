ca3e6e29569abe3731f92e20d9f4ea12c190a37f
Allen You <allenchanyou@gmail.com>
2022-02-15 15:09:59 +0000

    monitoring script

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ $# -ne 5 ]; then
+        echo "Invalid number of agruments"
+        echo "psql_host psql_port db_name psql_user psql_password"
+        exit 1
+fi
+
+lscpu_out=`lscpu`
+
+hostname=$(hostname -f)
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | cut -f 1 -d "K" | xargs)
+total_mem=$(cat /proc/meminfo | egrep "^MemTotal:" | awk '{print $2}' | xargs)
+
+timestamp=$(vmstat -t | awk '{print $18" "$19}' | tail -n1 | xargs)
+
+insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, timestamp) VALUES('$hostname','$cpu_number','$cpu_architecture','$cpu_model','$cpu_mhz','$l2_cache','$total_mem','$timestamp');"
+
+export PGPASSWORD=$psql_pasword
+
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
